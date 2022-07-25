5ce9bab9f8f4368dbbd4d5952bd4b6b5b2682520
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2022-07-26 02:10:25 +0300

    mobile-menu added by js

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-const hambuger= document.querySelector('.hambuger');
+const hambuger = document.querySelector('.hambuger');
 
 const navMenu = document.querySelector('.nav-menu');
 
-const navLinks = document.querySelectorAll('.nav-link');
+// const navLinks = document.querySelectorAll(".nav-link");
 
 hambuger.addEventListener('click', () => {
-    hambuger.classList.add('active');
+  hambuger.classList.toggle('active');
 
-  navMenu.classList.add('active');
+  navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
-    hambuger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
-
-// navLinks.forEach((link) => {
-//   link.addEventListener('click', () => {
-//     hambugerMenu.classList.remove('active');
-
-//     navMenu.classList.remove('active');
-//   });
-// });
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hambuger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
